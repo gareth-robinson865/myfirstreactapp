bf1b10dbc79d612552ba95fbff6aaac4b2736d3d
Gareth Robinson <gazz_1998@hotmail.co.uk>
2020-11-27 13:26:21 +0000

    function to log hello added and given to button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const sayHello = () => {
+  console.log("Hello");
+}
+
 function app() {
   return(
     <div>
       <h1>Hello React</h1>
+      <button>{sayHello}</button>
     </div>
   );
 }
