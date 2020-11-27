1cf9ea951c79d5a7e6afb954f0e7688ede8e78b4
Gareth Robinson <gazz_1998@hotmail.co.uk>
2020-11-27 14:26:11 +0000

    three section navbar created with test function

---
+++ src/navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function navBar() {
+
+    let clickToAddOne = () => {
+        let num = 0;
+        num++;
+    }
+
+    return(
+        <nav id="navbar">
+            <div id="logo">
+                <h1>This is where the logo goes</h1>
+            </div>
+            <div id="title">
+                <h1>This is where the title goes</h1>
+            </div>
+            <div id="navigation">
+                <button onClick={clickToAddOne}>Press the button</button>
+                <button onClick={clickToAddOne}>Push the button</button>
+                <button onClick={clickToAddOne}>Hold the button</button>
+            </div>
+        </nav>
+    );
+}
+
+export default navBar
