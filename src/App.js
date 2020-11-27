4d9e38ffa65404d2ce670beb578da539b558202d
Gareth Robinson <gazz_1998@hotmail.co.uk>
2020-11-27 13:34:37 +0000

    spacing changed in code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ const sayHello = () => {
   console.log("Hello");
 }
 
+
+
 function app() {
   return(
     <div>
       <h1>Hello React</h1>
-      <button onClick={sayHello}>Hello</button>
+      <button onClick={sayHello}>The number of clicks is {counter}</button>
     </div>
   );
 }
